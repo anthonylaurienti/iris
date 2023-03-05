f98fdf684e3298739ded1e28337c071280c703ae
Alfaya <fcalfaya@gmail.com>
2023-03-05 05:06:49 -0300

    all trasnlated

--- repository/patient.repository.js
+++ repository/patient.repository.js
@@ -1,12 +1,24 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const tableModel = `CREATE TABLE patients (id INT, name TEXT, devID TEXT,providerID TEXT, lastSync DATETIME)`
+const tableModel = 'CREATE TABLE patients (id INT, name TEXT )'
+
+
+// need to rename the data, check and type fields becouse they are reserved
+const tableModelIMG = `CREATE TABLE images (empty INTEGER,
+                        Pid INTEGER,
+                        DataSended TEXT,
+                        devID TEXT,
+                        TypeIntegration TEXT,
+                        sessionID INTEGER,
+                        CheckIntegration TEXT,
+                        Meta TEXT,
+                        Image TEXT )`
 
 
 
 function createTables(){
     let db = new sqlite3.Database('./db/irisDB');
-    sqlchecktable =`SELECT
+    let sqlchecktable =`SELECT
                         IIF(count(0)>0,1,0) exist
                     FROM
                         sqlite_master
@@ -20,18 +32,9 @@ function createTables(){
         }
         if(row.exist != 1){
             console.log("Criando tabela!")
-            db.run('CREATE TABLE patients (id INT, name TEXT )');
+            db.run(tableModel);
 
-            // need to rename the data, check and type fields becouse they are reserved
-            db.run(`CREATE TABLE images (empty INTEGER,
-                Pid INTEGER,
-                DataSended TEXT,
-                devID TEXT,
-                TypeIntegration TEXT,
-                sessionID INTEGER,
-                CheckIntegration TEXT,
-                Meta TEXT,
-                Image TEXT )`);
+            db.run(tableModelIMG);
         }
         console.log("Banco Ok!")
     });
@@ -39,7 +42,7 @@ function createTables(){
 }
 
 
-function saveimageData(imageData){
+function saveimageData(imageData,filePath){
     let db = new sqlite3.Database('./db/irisDB');
     let sql = `insert into images VALUES(
         ${imageData.empty},
@@ -50,7 +53,7 @@ function saveimageData(imageData){
         ${imageData.sessionID},
         '${imageData.Check}',
         '${imageData.meta}',
-        '${imageData.ImageName}'
+        '${filePath}'
     )`
 
     db.run(sql);
@@ -58,7 +61,60 @@ function saveimageData(imageData){
 }
 
 
+function getLast(devId,callback){
+
+    let response = []
+    let sql=`select
+            *
+        from
+            images
+        where 
+            devID = ${devId}
+        order by DataSended DESC 
+        limit 1`
+    DataExt(sql, function(err, content) {
+        if(err) throw(err);
+        callback(content[0])
+    })
+}
+
+function getImages(pid,sessionID,devID,callback){
+    let sql =`select
+          *
+        from
+          images
+        where 
+          Pid = ${pid}
+          and 
+            sessionID = ${sessionID}
+          and 
+            devID = ${devID}` 
+    DataExt(sql, function(err, content) {
+        if(err) throw(err);
+        callback(content)
+    })
+}
 
 
+function DataExt(sql, callback) {
+    let db = new sqlite3.Database('./db/irisDB');
 
-module.exports = { createTables,saveimageData};
+    db.all(sql,[],(err,rows)=>{
+        let rep = []
+        if(err) throw(err);
+
+        const tamanho = rows.length
+        
+        rows.forEach((row,key)=>{
+            rep.push(row);
+            if(key == tamanho-1){
+                db.close();
+                return callback(false, rep);
+            }
+        });
+        
+    });
+}
+
+
+module.exports = { createTables,saveimageData,getLast,getImages};
--- services/image.service.js
+++ services/image.service.js
@@ -2,23 +2,15 @@ const Image = require('../models/image');
 const qrcode = require('qrcode');
 const os = require('os');
 require('dotenv').config();
-const {saveimageData} = require("../repository/patient.repository")
+const {saveimageData, getLast, getImages} = require("../repository/patient.repository")
 const port = process.env.IRIS_PORT || 3000
 const fs = require('fs');
 
-const saveImage = async (imageData,imageName) => {
+const saveImage = async (imageData) => {
   try {
-    const filePath = `./images/${imageName}`;
-    saveimageData(imageData)
-  } catch (error) {
-    throw error;
-  }
-};
-
-const deleteImage = async (query) => {
-  try {
-    const deletedImage = await Image.deleteOne(query);
-    return deletedImage;
+    const ImageName = imageData.ImageName
+    const filePath = `/images/${ImageName}`;
+    await saveimageData(imageData,filePath)
   } catch (error) {
     throw error;
   }
@@ -44,26 +36,38 @@ const getWanIp = () => {
   return wanIp
 }
 
-const syncDate = async (deviceID, providerID) => {
+const syncDate = async (deviceID,providerID,callback) => {
   try {
-    const lastRegister = await Image.findOne({ devID: deviceID }).sort({Data: -1});
-    if (!lastRegister) {
-      throw new Error("No register found");
-    }
-    return {
-      devID: deviceID,
-      providerID: providerID,
-      lastSync: lastRegister.Data
-    };
+    getLast(deviceID,(lastRegister)=>{
+      if (!lastRegister) {
+        throw new Error("No register found");
+      }
+      callback({
+        devID: deviceID,
+        providerID: providerID,
+        lastSync: lastRegister.DataSended
+      });
+    });
   } catch (err) {
     throw err;
   }
 };
 
+const findImage = (pid,sessionID,devID, callback)=> {
+  try {
+    getImages(pid,sessionID,devID,(response)=>{
+      callback(response)
+    })
+  } catch (error) {
+    throw new Error("No register found");
+  }
+
+}
+
 module.exports = {
   saveImage,
-  deleteImage,
   generateQRCode,
   getWanIp,
-  syncDate
+  syncDate,
+  findImage
 };
